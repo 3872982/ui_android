b3459b8caf4145c24b354a979850e6a9fee0ee5e
Richen.Xu <490313997@qq.com>
2021-03-22 18:41:42 +0800

    利用XferMode实现刮刮乐效果

--- app/src/main/java/com/learning/ui/MainActivity.java
+++ app/src/main/java/com/learning/ui/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.learning.XMode.XModeView;
+import com.learning.XMode.XfermodeEraserView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GradientLayout(this));
+//        setContentView(new GradientLayout(this));
+//        setContentView(new XModeView(this));
+        setContentView(new XfermodeEraserView(this));
     }
 }
